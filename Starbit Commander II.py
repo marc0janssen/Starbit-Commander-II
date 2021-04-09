ac7f0400633d81e7a40912bd13b810a24329a7f1
Marco <5788988+marc0janssen@users.noreply.github.com>
2021-04-09 13:11:40 +0200

    Update Starbit Commander II.py

--- Starbit Commander II.py
+++ Starbit Commander II.py
@@ -1,5 +1,5 @@
 # Name: Starbit Commander II
-# Coder: Marco Janssen (twitter @marc0janssen, mail: marco@mjanssen.nl)
+# Coder: Marco Janssen (twitter @marc0janssen)
 # year: 2021-04-07
 # version: 1.0
 # notes: Explosion code borrowed with permission from Derek Graham.
