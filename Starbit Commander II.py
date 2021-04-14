6df1b448ecab6235a1d07cda7b16915fad5ac768
Marco <5788988+marc0janssen@users.noreply.github.com>
2021-04-14 20:45:07 +0200

    commit on main

--- Starbit Commander II.py
+++ Starbit Commander II.py
@@ -5,6 +5,8 @@
 # notes: Explosion code borrowed with permission from Derek Graham.
 # notes: https://deejaygraham.github.io/2016/10/28/tiny-asteroids-for-microbit/
 # notes: All other code is my own.
+# notes: This code will only run on v2 versions of the microbit
+
 
 from microbit import display, button_a, button_b, sleep
 from random import randint, randrange
